6d95a170ad40d82e3543d40efd74b64afcbe695c
Etienne <cp-eti2@hotmail.f>
2018-06-07 15:50:42 +0200

    ajout code exemple de celene

---
+++ code/CodesExemples/application/ChargeurJeu.java
@@ -0,0 +1,22 @@
+package application;
+import java.io.*;
+import java.net.*;
+import projet.Jeu;
+
+public class ChargeurJeu{
+    private String repertoireJar;
+    ChargeurJeu(String repertoireJar){
+	if (repertoireJar.endsWith("/"))
+	    this.repertoireJar=repertoireJar;
+	else
+	    this.repertoireJar=repertoireJar+"/";
+    }
+
+    @SuppressWarnings("deprecation")
+    Jeu chargerJeu(String nomJar, String nomClasse) throws ClassNotFoundException,InstantiationException,IllegalAccessException,MalformedURLException {
+ 	File ficJar = new File(this.repertoireJar+nomJar);
+	URL[] listeURL = {ficJar.toURL()};
+	ClassLoader loader = new URLClassLoader(listeURL);
+	return (Jeu)Class.forName(nomClasse,true,loader).newInstance();
+    }
+}
---
+++ code/CodesExemples/application/TelechargerJeu.java
@@ -0,0 +1,11 @@
+package application;
+
+public class TelechargerJeu {
+
+    public static void main(String[] args) throws Exception{
+        ConnexionMySQL laConnexion;
+        System.out.println(args[0]);
+        laConnexion=new ConnexionMySQL(args[0],args[1],args[2],args[3]);
+        laConnexion.getJar(args[4],args[5]);
+    }
+}
