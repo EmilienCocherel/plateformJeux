8b020834ad46dcc051ac3bce0e66751743c5d6cb
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-11 13:45:24 +0200

    PUISSANCE 4: pas besoin de gérer le cas d'un coup impossible car l'interface les interdit

--- code/puissance4/src/Plateau.java
+++ code/puissance4/src/Plateau.java
@@ -9,22 +9,16 @@ public class Plateau extends Matrice<Integer> {
 	/**
 	 * @param colonne La colonne à laquelle placer le pion
 	 * @param pion Le pion à placer
-	 * @return -1 si placer le pion a échoué, 0 si personne n'a gagné ou 1 si le pion a gagné la partie
+	 * @return si le pion a gagné la partie
 	 */
-    public int placerPion(int colonne, Integer pion) {
+    public boolean placerPion(int colonne, Integer pion) {
 		int ligne = 0;
-		if (this.caseLibre(colonne)) {
-			while (ligne+1 < this.getNbLignes() && this.get(colonne, ligne+1) == null)
-				ligne ++;
-			this.set(colonne, ligne, pion);
-		} else
-			return -1;
+		while (ligne+1 < this.getNbLignes() && this.get(colonne, ligne+1) == null)
+			ligne ++;
+		this.set(colonne, ligne, pion);
 
 		// Voir si le pion a gagné
-		if (this.aGagne(colonne, ligne))
-			return 1;
-		else
-			return 0;
+		return this.aGagne(colonne, ligne);
     }
 
 	public boolean aGagne(int colonne, int ligne) {
