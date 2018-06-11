85abafff667a967cf09aec13d5994a6860a53cef
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-11 09:47:00 +0200

    PUISSANCE 4: Plateau ajouter une vérification de victoire à placerPion(colonne, pion)

--- code/puissance4/src/Plateau.java
+++ code/puissance4/src/Plateau.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Plateau extends Matrice<Integer> {
     public Plateau() {
         super(7, 7);
@@ -7,16 +9,22 @@ public class Plateau extends Matrice<Integer> {
 	/**
 	 * @param colonne La colonne à laquelle placer le pion
 	 * @param pion Le pion à placer
+	 * @return -1 si placer le pion a échoué, 0 si personne n'a gagné ou 1 si le pion a gagné la partie
 	 */
-    public boolean placerPion(int colonne, Integer pion) {
+    public int placerPion(int colonne, Integer pion) {
+		int ligne = 0;
 		if (this.caseLibre(colonne)) {
-			int ligne = 0;
 			while (ligne+1 < this.getNbLignes() && this.get(colonne, ligne+1) == null)
 				ligne ++;
 			this.set(colonne, ligne, pion);
-			return true;
 		} else
-			return false;
+			return -1;
+
+		// Voir si le pion a gagné
+		if (this.aGagne(colonne, ligne))
+			return 1;
+		else
+			return 0;
     }
 
 	/**
