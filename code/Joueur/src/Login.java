d3c06420ed35a521ebe62be39f5c36c17b623dd5
Cocherel <emilien.cocherel@gmail.com>
2018-06-11 15:54:13 +0200

    Finalisation Vue Login

--- code/Joueur/src/Login.java
+++ code/Joueur/src/Login.java
@@ -37,7 +37,6 @@ public class Login extends Application {
 
         Menu seconnecter = new Menu("Se connecter");
 
-
         Menu creerCompte = new Menu("Créer un compte");
 
         Menu quitter = new Menu("Quitter");
@@ -46,7 +45,7 @@ public class Login extends Application {
 
         Stop[] stops = new Stop[] { new Stop(0.4, Color.BLACK), new Stop(1, Color.rgb(123,41,67))};
         LinearGradient lg1 = new LinearGradient(0, 1, 0, 0, true, CycleMethod.REPEAT, stops);
-        res.setBackground(new Background(new BackgroundFill(lg1, null, null)));
+        menu.setBackground(new Background(new BackgroundFill(lg1, null, null)));
 
         res.getChildren().addAll(menu);
 
@@ -57,37 +56,59 @@ public class Login extends Application {
     public GridPane connexion() {
         GridPane res = new GridPane();
 
+        Font fonttitre = new Font("Arial", 25);
+
+
         Label title = new Label("Connexion");
-        title.setTextFill(Color.web("#E50000"));
-        title.setPadding(new Insets(5, 0, 20, 5));
+        title.setTextFill(Color.rgb(179, 71, 91));
+        title.setPadding(new Insets(5, 0, 20, 10));
+        title.setFont(fonttitre);
         title.setAlignment(Pos.TOP_LEFT);
 
         HBox Hlogin = new HBox();
         Label Llogin = new Label("Email ou login : ");
+        Llogin.setTextFill(Color.rgb(196, 196, 196));
         TextField Tlogin = new TextField();
         Hlogin.getChildren().addAll(Llogin, Tlogin);
         Hlogin.setPadding(new Insets(20, 0, 10, 20));
-        Hlogin.setMinSize(400, 50);
 
+        //51, 52, 57
         HBox Hmdp = new HBox();
-        Label Lmdp = new Label("Mot de passe : ");
+        Label Lmdp = new Label("Mot de passe :  ");
+        Lmdp.setTextFill(Color.rgb(196, 196, 196));
         PasswordField Tmdp = new PasswordField();
         Hmdp.getChildren().addAll(Lmdp, Tmdp);
-        Hmdp.setPadding(new Insets(20, 0, 10, 20));
-        Hmdp.setMinSize(400, 50);
+        Hmdp.setPadding(new Insets(10, 0, 0, 20));
+
+        CheckBox cb = new CheckBox("Enregistrer mot de passe");
+        cb.setTextFill(Color.rgb(196, 196, 196));
+        cb.setPadding(new Insets(10, 0, 0, 20));
 
 
-        CheckBox cb = new CheckBox();
-        Label lenregistrer = new Label("Enregistrer mot de passe");
+        HBox Hbouton = new HBox();
+        Button bconnect = new Button("Se connecter");
+        bconnect.setTextFill(Color.rgb(196, 196, 196));
+        bconnect.setBackground(new Background(new BackgroundFill(Color.rgb(179, 71, 91), new CornerRadii(5, false), null)));
+        bconnect.setPadding(new Insets(5, 50, 5,50));
+        Hbouton.setPadding(new Insets(40, 0, 0, 100));
+        Hbouton.getChildren().addAll(bconnect);
 
-        res.setBackground(new Background(new BackgroundFill(Color.GRAY, new CornerRadii(5, false), Insets.EMPTY)));
+        HBox Hmdpoublie = new HBox();
+        Label Lmdpoublie = new Label("Mot de passe oublié ?");
+        Lmdpoublie.setTextFill(Color.rgb(179, 71, 91));
+        Hmdpoublie.getChildren().addAll(Lmdpoublie);
+        Hmdpoublie.setPadding(new Insets(10, 0, 0, 100));
+
+
+        res.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
         res.setMaxSize(400, 300);
 
         res.add(title, 0, 0);
         res.add(Hlogin, 0, 1);
         res.add(Hmdp, 0, 2);
-        res.add(cb, 1, 3);
-        res.add(lenregistrer, 1, 4);
+        res.add(cb, 0, 3);
+        res.add(Hbouton, 0, 4);
+        res.add(Hmdpoublie, 0, 5);
 
         return res;
 
