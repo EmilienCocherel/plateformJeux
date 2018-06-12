9bacdb2a2dc4147a51e071de50907e6403039490
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-12 10:51:11 +0200

    PUISSANCE 4: maj lancer.sh : ne lance pas le puissance 4 si la compilation n'a pas réussi

--- code/puissance4/lancer.sh
+++ code/puissance4/lancer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+export CLASSPATH=$CLASSPATH:./bin
+if [ -f Connect4.jar ]; then
+	rm Connect4.jar
+fi
 mkdir -p bin
-javac -d bin src/*.java
-cd bin
-jar cfe ../Connect4.jar GUI *.class
-cd ..
-java -jar Connect4.jar
+javac src/*.java
+if (( $? == 0 )); then
+	cd bin
+	jar cfe ../Connect4.jar GUI *.class
+	cd ..
+	java -jar Connect4.jar
+fi
