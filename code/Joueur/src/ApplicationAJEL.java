5f7e261c38f29179c3bef8bee93d5da02d039498
Quentin BERNARD <quentin.bernard3@etu.univ-orleans.fr>
2018-06-12 14:41:47 +0200

    changement en label/actionbout/Application

--- code/Joueur/src/ApplicationAJEL.java
+++ code/Joueur/src/ApplicationAJEL.java
@@ -1,45 +1,35 @@
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TextField;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonBar.ButtonData;
-import javafx.scene.control.CheckBox;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import static javafx.application.Application.launch;
 
-public class CreeCompte extends Application {
-
+public class ApplicationAJEL extends Application {
     Menu seconnecter,creerCompte;
+    Stage window;
+    Scene seco,creer;
+    VBox res,res2;
+    MenuItem compte;
 
     public VBox menuBar() {
-        VBox res = new VBox();
+        res = new VBox();
+        res2 = new VBox();
 
         MenuBar menu = new MenuBar();
 
-         this.seconnecter = new Menu("Se connecter");
-
-         this.creerCompte = new Menu("Créer un compte");
+        this.seconnecter = new Menu("Se connecter");
+        this.compte = new MenuItem("Aller");
+        this.seconnecter.getItems().add(this.compte);
+        this.creerCompte = new Menu("Créer un compte");
 
         Menu quitter = new Menu("Quitter");
 
@@ -124,8 +114,78 @@ public class CreeCompte extends Application {
         return res;
     }
 
+    public BorderPane leBorder() {
+        GridPane res2 = new GridPane();
 
-    private Scene laScene(){
+        Font fonttitre = new Font("Arial", 25);
+
+
+        Label title = new Label("Connexion");
+        title.setTextFill(Color.rgb(179, 71, 91));
+        title.setPadding(new Insets(5, 0, 20, 10));
+        title.setFont(fonttitre);
+        title.setAlignment(Pos.TOP_LEFT);
+
+        HBox Hlogin = new HBox();
+        Label Llogin = new Label("Email ou login : ");
+        Llogin.setTextFill(Color.rgb(196, 196, 196));
+        TextField Tlogin = new TextField();
+        Hlogin.getChildren().addAll(Llogin, Tlogin);
+        Hlogin.setPadding(new Insets(20, 0, 10, 20));
+
+        HBox Hmdp = new HBox();
+        Label Lmdp = new Label("Mot de passe :  ");
+        Lmdp.setTextFill(Color.rgb(196, 196, 196));
+        PasswordField Tmdp = new PasswordField();
+        Hmdp.getChildren().addAll(Lmdp, Tmdp);
+        Hmdp.setPadding(new Insets(10, 0, 0, 20));
+
+        CheckBox cb = new CheckBox("Enregistrer mot de passe");
+        cb.setTextFill(Color.rgb(196, 196, 196));
+        cb.setPadding(new Insets(10, 0, 0, 20));
+
+
+        HBox Hbouton = new HBox();
+        Button bconnect = new Button("Se connecter");
+        bconnect.setTextFill(Color.rgb(196, 196, 196));
+        bconnect.setBackground(new Background(new BackgroundFill(Color.rgb(179, 71, 91), new CornerRadii(5, false), null)));
+        bconnect.setPadding(new Insets(5, 50, 5, 50));
+        Hbouton.setPadding(new Insets(40, 0, 0, 100));
+        Hbouton.getChildren().addAll(bconnect);
+
+        HBox Hmdpoublie = new HBox();
+        Label Lmdpoublie = new Label("Mot de passe oublié ?");
+        Lmdpoublie.setTextFill(Color.rgb(179, 71, 91));
+        Hmdpoublie.getChildren().addAll(Lmdpoublie);
+        Hmdpoublie.setPadding(new Insets(10, 0, 0, 100));
+
+
+        res2.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
+        res2.setMaxSize(400, 300);
+
+        res2.add(title, 0, 0);
+        res2.add(Hlogin, 0, 1);
+        res2.add(Hmdp, 0, 2);
+        res2.add(cb, 0, 3);
+        res2.add(Hbouton, 0, 4);
+        res2.add(Hmdpoublie, 0, 5);
+
+        BorderPane cont = new BorderPane();
+        cont.setTop(this.menuBar());
+        cont.setCenter(res2);
+        Stop[] stops2 = new Stop[]{new Stop(0.4, Color.BLACK), new Stop(1, Color.rgb(123, 41, 67))};
+        LinearGradient lg2 = new LinearGradient(0, 1, 0, 0, true, CycleMethod.REPEAT, stops2);
+
+        cont.setBackground(new Background(new BackgroundFill(lg2, null, null)));
+        return cont;
+    }
+
+
+
+    @Override
+    public void start(Stage stage) {
+        window = stage;
+
         BorderPane cont = new BorderPane();
         cont.setTop(this.menuBar());
         cont.setCenter(this.creerCompte());
@@ -133,15 +193,16 @@ public class CreeCompte extends Application {
         LinearGradient lg1 = new LinearGradient(0, 1, 0, 0, true, CycleMethod.REPEAT, stops);
 
         cont.setBackground(new Background(new BackgroundFill(lg1,null,null)));
-        return new Scene(cont,800,600);
-    }
+        this.creer = new Scene(cont,800,600);
 
-    @Override
-    public void start(Stage stage) {
 
-        stage.setTitle("AJEL");
-        stage.setScene(this.laScene());
-        stage.show();
+        this.seco = new Scene(this.leBorder(),800,600);
+
+        this.seconnecter.setOnAction(event -> window.setScene(this.creer));
+        this.creerCompte.setOnAction(event -> window.setScene(this.seco));
+        window.setScene(this.seco);
+        window.setTitle("AJEL");
+        window.show();
 
     }
 
--- code/Joueur/src/Login.java
+++ code/Joueur/src/Login.java
@@ -28,112 +28,96 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Login extends Application {
+public class Login extends BorderPane {
+    private BorderPane sceneLogin;
 
-    public VBox menuBar() {
-        VBox res = new VBox();
+    public Login() {
 
-        MenuBar menu = new MenuBar();
+            VBox res = new VBox();
 
-        Menu seconnecter = new Menu("Se connecter");
+            MenuBar menu = new MenuBar();
 
-        Menu creerCompte = new Menu("Créer un compte");
+            Menu seconnecter = new Menu("Se connecter");
 
-        Menu quitter = new Menu("Quitter");
+            Menu creerCompte = new Menu("Créer un compte");
 
-        menu.getMenus().addAll(seconnecter, creerCompte, quitter);
+            Menu quitter = new Menu("Quitter");
 
-        Stop[] stops = new Stop[] { new Stop(0.4, Color.BLACK), new Stop(1, Color.rgb(123,41,67))};
-        LinearGradient lg1 = new LinearGradient(0, 1, 0, 0, true, CycleMethod.REPEAT, stops);
-        menu.setBackground(new Background(new BackgroundFill(lg1, null, null)));
+            menu.getMenus().addAll(seconnecter, creerCompte, quitter);
 
-        res.getChildren().addAll(menu);
+            Stop[] stops1 = new Stop[]{new Stop(0.4, Color.BLACK), new Stop(1, Color.rgb(123, 41, 67))};
+            LinearGradient lg1 = new LinearGradient(0, 1, 0, 0, true, CycleMethod.REPEAT, stops1);
+            menu.setBackground(new Background(new BackgroundFill(lg1, null, null)));
 
-        return res;
-
-    }
-
-    public GridPane connexion() {
-        GridPane res = new GridPane();
-
-        Font fonttitre = new Font("Arial", 25);
+            res.getChildren().addAll(menu);
 
 
-        Label title = new Label("Connexion");
-        title.setTextFill(Color.rgb(179, 71, 91));
-        title.setPadding(new Insets(5, 0, 20, 10));
-        title.setFont(fonttitre);
-        title.setAlignment(Pos.TOP_LEFT);
+            GridPane res2 = new GridPane();
 
-        HBox Hlogin = new HBox();
-        Label Llogin = new Label("Email ou login : ");
-        Llogin.setTextFill(Color.rgb(196, 196, 196));
-        TextField Tlogin = new TextField();
-        Hlogin.getChildren().addAll(Llogin, Tlogin);
-        Hlogin.setPadding(new Insets(20, 0, 10, 20));
-
-        HBox Hmdp = new HBox();
-        Label Lmdp = new Label("Mot de passe :  ");
-        Lmdp.setTextFill(Color.rgb(196, 196, 196));
-        PasswordField Tmdp = new PasswordField();
-        Hmdp.getChildren().addAll(Lmdp, Tmdp);
-        Hmdp.setPadding(new Insets(10, 0, 0, 20));
-
-        CheckBox cb = new CheckBox("Enregistrer mot de passe");
-        cb.setTextFill(Color.rgb(196, 196, 196));
-        cb.setPadding(new Insets(10, 0, 0, 20));
+            Font fonttitre = new Font("Arial", 25);
 
 
-        HBox Hbouton = new HBox();
-        Button bconnect = new Button("Se connecter");
-        bconnect.setTextFill(Color.rgb(196, 196, 196));
-        bconnect.setBackground(new Background(new BackgroundFill(Color.rgb(179, 71, 91), new CornerRadii(5, false), null)));
-        bconnect.setPadding(new Insets(5, 50, 5,50));
-        Hbouton.setPadding(new Insets(40, 0, 0, 100));
-        Hbouton.getChildren().addAll(bconnect);
+            Label title = new Label("Connexion");
+            title.setTextFill(Color.rgb(179, 71, 91));
+            title.setPadding(new Insets(5, 0, 20, 10));
+            title.setFont(fonttitre);
+            title.setAlignment(Pos.TOP_LEFT);
 
-        HBox Hmdpoublie = new HBox();
-        Label Lmdpoublie = new Label("Mot de passe oublié ?");
-        Lmdpoublie.setTextFill(Color.rgb(179, 71, 91));
-        Hmdpoublie.getChildren().addAll(Lmdpoublie);
-        Hmdpoublie.setPadding(new Insets(10, 0, 0, 100));
+            HBox Hlogin = new HBox();
+            Label Llogin = new Label("Email ou login : ");
+            Llogin.setTextFill(Color.rgb(196, 196, 196));
+            TextField Tlogin = new TextField();
+            Hlogin.getChildren().addAll(Llogin, Tlogin);
+            Hlogin.setPadding(new Insets(20, 0, 10, 20));
+
+            HBox Hmdp = new HBox();
+            Label Lmdp = new Label("Mot de passe :  ");
+            Lmdp.setTextFill(Color.rgb(196, 196, 196));
+            PasswordField Tmdp = new PasswordField();
+            Hmdp.getChildren().addAll(Lmdp, Tmdp);
+            Hmdp.setPadding(new Insets(10, 0, 0, 20));
+
+            CheckBox cb = new CheckBox("Enregistrer mot de passe");
+            cb.setTextFill(Color.rgb(196, 196, 196));
+            cb.setPadding(new Insets(10, 0, 0, 20));
 
 
-        res.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
-        res.setMaxSize(400, 300);
+            HBox Hbouton = new HBox();
+            Button bconnect = new Button("Se connecter");
+            bconnect.setTextFill(Color.rgb(196, 196, 196));
+            bconnect.setBackground(new Background(new BackgroundFill(Color.rgb(179, 71, 91), new CornerRadii(5, false), null)));
+            bconnect.setPadding(new Insets(5, 50, 5, 50));
+            Hbouton.setPadding(new Insets(40, 0, 0, 100));
+            Hbouton.getChildren().addAll(bconnect);
 
-        res.add(title, 0, 0);
-        res.add(Hlogin, 0, 1);
-        res.add(Hmdp, 0, 2);
-        res.add(cb, 0, 3);
-        res.add(Hbouton, 0, 4);
-        res.add(Hmdpoublie, 0, 5);
+            HBox Hmdpoublie = new HBox();
+            Label Lmdpoublie = new Label("Mot de passe oublié ?");
+            Lmdpoublie.setTextFill(Color.rgb(179, 71, 91));
+            Hmdpoublie.getChildren().addAll(Lmdpoublie);
+            Hmdpoublie.setPadding(new Insets(10, 0, 0, 100));
 
-        return res;
 
-    }
+            res.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
+            res.setMaxSize(400, 300);
 
-    private Scene laScene(){
-        BorderPane cont = new BorderPane();
-        cont.setTop(this.menuBar());
-        cont.setCenter(this.connexion());
-        Stop[] stops = new Stop[] { new Stop(0.4, Color.BLACK), new Stop(1, Color.rgb(123,41,67))};
-        LinearGradient lg1 = new LinearGradient(0, 1, 0, 0, true, CycleMethod.REPEAT, stops);
+            res2.add(title, 0, 0);
+            res2.add(Hlogin, 0, 1);
+            res2.add(Hmdp, 0, 2);
+            res2.add(cb, 0, 3);
+            res2.add(Hbouton, 0, 4);
+            res2.add(Hmdpoublie, 0, 5);
 
-        cont.setBackground(new Background(new BackgroundFill(lg1,null,null)));
-        return new Scene(cont,800,600);
-    }
+            BorderPane cont = new BorderPane();
+            cont.setTop(res);
+            cont.setCenter(res2);
+            Stop[] stops2 = new Stop[]{new Stop(0.4, Color.BLACK), new Stop(1, Color.rgb(123, 41, 67))};
+            LinearGradient lg2 = new LinearGradient(0, 1, 0, 0, true, CycleMethod.REPEAT, stops2);
 
-    @Override
-    public void start(Stage stage) {
+            cont.setBackground(new Background(new BackgroundFill(lg2, null, null)));
+            this.sceneLogin=cont;
+        }
 
-        stage.setTitle("AJEL");
-        stage.setScene(this.laScene());
-        stage.show();
-
-    }
-
-    public static void main(String[] args) {
-        launch(args);
+    public BorderPane getPanelLogin() {
+        return this.sceneLogin;
     }
 }
