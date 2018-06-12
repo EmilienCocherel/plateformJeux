1e211391124c977493c08293e3588c695d1b77cd
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-12 15:10:22 +0200

    PUISSANCE 4: màj lancer.sh & compiler.sh basé sur le commit précédent

--- code/puissance4/compiler.sh
+++ code/puissance4/compiler.sh
@@ -5,6 +5,6 @@ if [ -f Connect4.jar ]; then
 fi
 javac -d ./ src/*.java
 if (( $? == 0 )); then
-	jar -cvf Connect4.jar *.class style/style.css
-	rm *.class
+	jar -cvf Connect4.jar Connect4/*.class style/style.css
+	rm -r Connect4/
 fi
--- code/puissance4/lancer.sh
+++ code/puissance4/lancer.sh
@@ -5,7 +5,7 @@ if [ -f Connect4.jar ]; then
 fi
 javac -d ./ src/*.java
 if (( $? == 0 )); then
-	jar cfe Connect4.jar Connect4.LeJeu *.class style/style.css
-	rm *.class
+	jar cfe Connect4.jar Connect4.LeJeu Connect4/*.class style/style.css
+	rm -r Connect4/
 	java -jar Connect4.jar
 fi
