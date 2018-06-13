ac374f9d09d08615a21af814835c2fb011d64f41
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-13 11:29:13 +0200

    PUISSANCE 4: ActionJouer gérer la fin d'un round

--- code/puissance4/src/ActionJouer.java
+++ code/puissance4/src/ActionJouer.java
@@ -3,6 +3,9 @@ package Connect4;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 /**
  * Contrôleur du plateau
@@ -25,8 +28,17 @@ public class ActionJouer implements EventHandler<ActionEvent> {
 	@Override
 	public void handle(ActionEvent actionEvent) {
 		Button b = (Button) actionEvent.getSource();
+		Joueur courant = this.puissance4.getJoueurCourant();
 		int i = Integer.parseInt(b.getText())-1;
-		this.puissance4.jouer(i%7);
+		if (this.puissance4.jouer(i%7)) {
+			this.gui.majAffichage();
+			Alert alert = new Alert(AlertType.INFORMATION, "You've won round "+this.puissance4.getRound()+"!");
+			alert.showAndWait();
+			this.puissance4.getGagnants().add(courant);
+			this.puissance4.getJoueur1().reset();
+			this.puissance4.getJoueur2().reset();
+			this.puissance4.getPlateau().reset();
+		}
 		// TODO: envoyer l'état de la partie à la bdd
 		this.gui.majAffichage();
 	}
