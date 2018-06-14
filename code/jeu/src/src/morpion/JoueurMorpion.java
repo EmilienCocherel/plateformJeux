2135393c3d8a28f1e347fd5810a8cf3354a7ee21
Etienne <cp-eti2@hotmail.f>
2018-06-14 15:37:33 +0200

    avant fusion

--- code/jeu/src/src/morpion/JoueurMorpion.java
+++ code/jeu/src/src/morpion/JoueurMorpion.java
@@ -3,29 +3,19 @@ package morpion;
 import java.util.ArrayList;
 import application.*;
 
-public class JoueurMastermind extends application.Joueur {
+public class JoueurMorpion extends application.Joueur {
     private int score;
-    private ArrayList<Manche> manches;
 
-    public JoueurMastermind(int id){
+    public JoueurMorpion(int id){
         this.identifiant=id;
         this.score=0;
-        this.manches=new ArrayList<>();
     }
 
     public void jouerPartie(){ // À IMPLÉMENTER
 
     }
 
-    public void nouvelleManche(Manche manche){
-        this.manches.add(manche);
-    }
-
 //    Getter et Setter
-
-    public Manche getMancheCourante(){
-        return this.manches.get(this.manches.size()-1);
-    }
 
     public int getScore(){return this.score;}
 
