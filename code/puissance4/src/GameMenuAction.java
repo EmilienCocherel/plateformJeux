35a0f157608a522b48017e554883c005aeef0853
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-14 10:31:49 +0200

    PUISSANCE 4: début d'implémentation d'afficheManches() (le dialogue peut pas être fermé)

--- code/puissance4/src/GameMenuAction.java
+++ code/puissance4/src/GameMenuAction.java
@@ -3,10 +3,18 @@ package Connect4;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.application.Platform;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.shape.Circle;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Dialog;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class GameMenuAction implements EventHandler<ActionEvent> {
 	/**
@@ -42,6 +50,7 @@ public class GameMenuAction implements EventHandler<ActionEvent> {
 		} else if (text.equals("Score tab")) {
 			// TODO
 			System.out.println("Score tab");
+			this.afficheManches();
 		}
 	}
 
@@ -49,6 +58,27 @@ public class GameMenuAction implements EventHandler<ActionEvent> {
 	 * Affiche les gagnants de chaque manche
 	 */
 	public void afficheManches() {
-		// TODO
+		Dialog fenetre = new Dialog();
+		DialogPane root = new DialogPane();
+		GridPane content = new GridPane();
+		List<Circle> pions = new ArrayList<>();
+		fenetre.initOwner(this.gui.getPlateau().getScene().getWindow());
+		fenetre.setDialogPane(root);
+		root.setContent(content);
+		for (int i=0; i < 3; i++) {
+			pions.add(new Circle(40));
+			pions.get(i).getStyleClass().addAll("pion", "no-pause");
+			if (this.puissance4.getRound()-1 > i) {
+				if (this.puissance4.getGagnants().get(i).getPion() == 1)
+					pions.get(i).setId("joueur1");
+				else
+					pions.get(i).setId("joueur2");
+				content.add(new Label(this.puissance4.getGagnants().get(i).getNom()), 2, i+1);
+			} else
+				content.add(new Label("Not yet"), 2, i+1);
+			content.add(pions.get(i), 1, i+1);
+		}
+		fenetre.showAndWait();
+		// TODO: gérer la fermeture du dialogue :(
 	}
 }
