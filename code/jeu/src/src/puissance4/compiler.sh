14b008158e6e01a70de96f9cdb184b86052c7060
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-14 10:42:31 +0200

    PUISSANCE 4: màj compiler.sh & lancer.sh

--- code/jeu/src/src/puissance4/compiler.sh
+++ code/jeu/src/src/puissance4/compiler.sh
@@ -3,7 +3,7 @@ export CLASSPATH=$CLASSPATH:./bin
 if [ -f Connect4.jar ]; then
 	rm Connect4.jar
 fi
-javac -d ./ src/*.java
+javac -d ./ *.java
 if (( $? == 0 )); then
 	jar -cvfm Connect4.jar Manifest.txt Connect4/*.class style/style.css
 	rm -r Connect4/
--- code/jeu/src/src/puissance4/lancer.sh
+++ code/jeu/src/src/puissance4/lancer.sh
@@ -2,7 +2,7 @@
 if [ -f Connect4.jar ]; then
 	rm Connect4.jar
 fi
-javac -d ./ src/*.java
+javac -d ./ *.java
 if (( $? == 0 )); then
 	jar cfme Connect4.jar Manifest.txt Connect4.LeJeu Connect4/*.class style/style.css
 	rm -r Connect4/
