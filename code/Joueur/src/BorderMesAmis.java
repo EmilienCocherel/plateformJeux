92fd7e218c85c6d0278961b2f7e76c213ef14dbb
Quentin BERNARD <quentin.bernard3@etu.univ-orleans.fr>
2018-06-14 14:44:29 +0200

    modification mesAmis

--- code/Joueur/src/BorderMesAmis.java
+++ code/Joueur/src/BorderMesAmis.java
@@ -1,5 +1,6 @@
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -15,14 +16,20 @@ public class BorderMesAmis extends PageJoueur {
         super();
         this.app = app;
 
-        HBox hboxtop= this.hBoxTypetextBouton("Demander en ami");
+        GridPane sceneTop = new GridPane();
+
+        sceneTop.add(this.buttonTypePageJoueur("Demander en ami"),0,0);
+        sceneTop.add(new TextField(),1,0);
+        sceneTop.setPadding(new Insets(10, 30, 10, 10));
 
         GridPane sceneCentre = new GridPane();
 
         sceneCentre.add(this.buttonBarTypePageJoueur("Mes amis","Mes demandes"),0,0);
         sceneCentre.add(this.tableauTypePageJouer("Nom","Niveau","Pourcentage victoire"),0,1);
+        sceneCentre.setPadding(new Insets(10, 30, 10, 10));
+        sceneCentre.setMinWidth(600);
 
-        this.setTop(hboxtop);
+        this.setTop(sceneTop);
         this.setCenter(sceneCentre);
         this.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
         this.setMaxSize(800, 700);
