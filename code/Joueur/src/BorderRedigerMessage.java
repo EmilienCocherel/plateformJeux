1aca3934d50ffdeb8361db58e09c6ec56f53b8fa
Cocherel <emilien.cocherel@gmail.com>
2018-06-14 15:00:48 +0200

    Commencement RedigerMessage

--- code/Joueur/src/BorderRedigerMessage.java
+++ code/Joueur/src/BorderRedigerMessage.java
@@ -1,3 +1,9 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class BorderRedigerMessage extends PageJoueur {
 
     AJEL app;
@@ -6,6 +12,38 @@ public class BorderRedigerMessage extends PageJoueur {
         super();
         this.app = app;
 
+        VBox vobjet = new VBox();
 
+        Label lobjet = new Label("Objet");
+        TextField tobjet = new TextField();
+
+        vobjet.getChildren().addAll(lobjet, tobjet);
+
+        VBox vdestinataire = new VBox();
+
+        Label ldesinataire = new Label("Destinataire");
+        TextField tdestinataire = new TextField("Destinataire");
+
+        vdestinataire.getChildren().addAll(ldesinataire, tdestinataire);
+
+        HBox haut = new HBox();
+        haut.getChildren().addAll(vobjet, vdestinataire);
+
+        VBox vmessage = new VBox();
+
+        Label lmessage = new Label("Message");
+        TextField tmessage = new TextField();
+
+        vmessage.getChildren().addAll(lmessage, tmessage);
+
+        Button bEnvoyer = this.buttonTypePageJoueur("Envoyer");
+        bEnvoyer.setOnAction(event -> this.app.passerEnModeMessagerieEnvoyes());
+
+        Button bAnnuler = this.buttonTypePageJoueur("Annuler");
+        bAnnuler.setOnAction(event -> this.app.passerEnModeMessagerieRecus());
+
+        this.setTop(haut);
+        this.setCenter(vmessage);
+        this.setBottom(this.buttonBarTypePageJoueur(bEnvoyer, bAnnuler));
     }
 }
