1f39c0ad2c7700cf6ad6daa3a3a1fbe061679a3f
Cocherel <emilien.cocherel@gmail.com>
2018-06-15 14:25:30 +0200

    fix

--- code/Adminsitrateur/src/AJEL.java
+++ code/Adminsitrateur/src/AJEL.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class AJEL extends Application {
+
+    private BorderPane laBase;
 
     public static void main(String[] args) {
         launch(args);
