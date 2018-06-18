9b30e82ce76e2e88a7383bc8bbe04e6d5fdc70de
Edwin CHARLOTTE <zed45000@gmail.com>
2018-06-18 13:16:48 +0200

    Suppresion accidentelle d'un fichier et récupération du travail réaliser

--- code/jeu/src/application/GridConnexion.java
+++ code/jeu/src/application/GridConnexion.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
@@ -8,9 +11,6 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class GridConnexion extends PageConnexion{
 
@@ -18,14 +18,17 @@ public class GridConnexion extends PageConnexion{
     private TextField email, mdp;
     private String login, motdepasse;
     private String type;
+    private Button seConnecterButton;
+    private ControleurConnexion cc;
+
 
     public GridConnexion(AppliJDBC appli){
         super();
         this.appli = appli;
+        this.cc = cc;
+        Button seConnecterBouton = this.buttonType("Se connecter");
 
-        Button seConnecter = this.buttonType("Se connecter");
 
-        seConnecter.setOnAction(event -> this.appli.passerEnModePartieEnCours());
 
         Hyperlink lienMDP = this.lienType("Mot de passe oublié ?");
         lienMDP.setOnAction(event -> this.appli.passerEnModeMDPOublie());
@@ -36,14 +39,20 @@ public class GridConnexion extends PageConnexion{
         this.mdp = new TextField("");
 
         this.add(this.title("Connexion"), 0, 0);
-        this.add(this.labelType("Email ou Pseudo : "), 0, 1);
+        this.add(this.labelType("Email : "), 0, 1);
         this.add(email,1,1);
         this.add(this.labelType("Mot de passe : "), 0, 2);
         this.add(mdp,1,2);
         this.add(this.checkBoxType("Enregistrer mot de passe"), 1, 3);
         this.add(this.labelType(""), 0, 4);
-        this.add(seConnecter, 1, 5);
+        this.add(seConnecterBouton, 1, 5);
         this.add(lienMDP, 1, 6);
+
+
+        // this.seConnecterButton.setOnAction(this.cc);
+        // this.seConnecterButton.setOnAction(event -> this.seConnecterBD());
+        seConnecterBouton.setOnAction(event -> this.seConnecterBD());
+
 
     }
 
@@ -54,5 +63,22 @@ public class GridConnexion extends PageConnexion{
     public String getMdp(){
       return this.mdp.getText();
     }
+
+    public boolean seConnecterBD(){
+      try{
+        System.out.println("Connexion réussie");
+        this.appli.getJoueurBD().seConnecter("'"+this.email.getText()+"'","'"+this.mdp.getText()+"'");
+        return true;
+      }
+      catch (SQLException e)
+      {
+        System.out.println(e);
+        return false;
+      }
+    }
+
+    // public Joueur connexion() throws SQLException{
+    //   return new Joueur(this.appli.getJoueurBD().seConnecter(this.email.getText(), this.mdp.getText()));
+    // }
 
 }
