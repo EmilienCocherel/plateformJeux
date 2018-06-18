7cc29468d661373689a5db3d3f24f6af611137bb
Quentin BERNARD <quentin.bernard3@etu.univ-orleans.fr>
2018-06-18 08:48:32 +0200

    ajout de BorderAjouterJeu

--- code/Administrateur/src/BorderAjouterJeu.java
+++ code/Administrateur/src/BorderAjouterJeu.java
@@ -1,4 +1,5 @@
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -13,6 +14,12 @@ public class BorderAjouterJeu extends PageAdmin {
     public BorderAjouterJeu(AJEL app){
         super();
         this.app = app;
+
+        Button bMenu = this.buttonTypePageAdmin("Accueil");
+        bMenu.setOnAction(event -> this.app.passerEnModeAccueil());
+
+        Button bEnregistrer = this.buttonTypePageAdmin("Enregistrer");
+        //bEnregistrer.setOnAction(event -> this.app.passerEnModeGererJeu());
 
         GridPane cotegauche = new GridPane();
 
@@ -30,13 +37,26 @@ public class BorderAjouterJeu extends PageAdmin {
 
         cotehaut.add(this.imageTypePageAdmin(),0,0);
         cotehaut.add(cotegauche,1,0);
+        cotehaut.setPadding(new Insets(30, 30, 10, 10));
+
+        GridPane cotebas = new GridPane();
+
+
+        cotebas.add(this.labelTypePageAdmin("Fichier : "),0,0);
+        cotebas.add(new TextField(),1,0);
+        cotebas.add(this.labelTypePageAdmin("Description : "),0,1);
+        cotebas.add(this.textAeraHorizontal(),1,1);
+        cotebas.setPadding(new Insets(30, 30, 10, 10));
 
 
 
-
+        this.setTop(cotehaut);
+        this.setCenter(cotebas);
+        this.setBottom(this.buttonBarTypePageAdmin(bMenu, bEnregistrer));
 
 
         this.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
+        this.setPadding(new Insets(0,20,0,20));
         this.setMaxSize(800, 700);
     }
 }
