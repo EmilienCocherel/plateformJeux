baaf198d0a288d4409217ac75234d371847ef7a1
Etienne <cp-eti2@hotmail.f>
2018-06-19 14:23:38 +0200

    suppression attribut val de pion

--- code/jeu/src/mastermind/Pion.java
+++ code/jeu/src/mastermind/Pion.java
@@ -18,23 +18,26 @@ public class Pion extends Circle{
 		put("orange", Color.ORANGE);
   }};
 
-  public Pion(Paint couleur,int val){
+  public Pion(Paint couleur){
     this.setRadius(10.0);
     this.setFill(couleur);
   }
 
-  public Pion(String coul,int val) {
+  public Pion(String coul) {
     this.setRadius(10.0);
-	this.setCouleur(coul);
+	  this.setCouleur(coul);
   }
 
-  public int getVal(){
-    return this.val;
-  }
+  //getter setter
 
   public Paint getCouleur(){
     return this.getFill();
   }
+
+  public void setCouleur(String coul) {
+		System.out.println(coul);
+		this.setFill(this.couleurs.get(coul));
+	}
 
 
   @Override
@@ -49,10 +52,5 @@ public class Pion extends Circle{
 			if (entry.getValue().equals(this.getCouleur()))
 				return entry.getKey();
 		return "";
-	}
-
-	public void setCouleur(String coul) {
-		System.out.println(coul);
-		this.setFill(this.couleurs.get(coul));
 	}
 }
