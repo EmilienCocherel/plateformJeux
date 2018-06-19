a583bac7a39ff31d19bc687ec41f254db2d21905
Etienne <cp-eti2@hotmail.f>
2018-06-19 11:37:15 +0200

    PUISSANCE4: problème taille about us (tout le texte n'est pas affiché

--- code/jeu/src/connect4/HelpMenuAction.java
+++ code/jeu/src/connect4/HelpMenuAction.java
@@ -24,10 +24,13 @@ public class HelpMenuAction implements EventHandler<ActionEvent> {
 		MenuItem menu = (MenuItem) actionEvent.getSource();
 		String text = menu.getText();
 		if (text.equals("About us")) {
-			Alert alert = new Alert(AlertType.INFORMATION, "");
+			Alert alert = new Alert(AlertType.INFORMATION, "Nous somme la team AJEL,\nC'est nous qui avons codé ce jeu avec le reste de la plateforme pour notre projet de fin d'année à l'IUT informatique d'Orléans.\nRejoignez nous du côté gris de la force.\nL'équipe se compose de:\nAurélien Desprez, Emilien Cocherel\nEdwin Charlotte, Quentin Bernard\nAmélie Dauvois, Etienne Fouquet");
 			alert.setTitle("About us");
 			alert.setHeaderText("About us");
 			alert.setGraphic(new ImageView("./img/about_us.gif"));
+			((ImageView)alert.getGraphic()).setFitHeight(60.0);
+			((ImageView)alert.getGraphic()).setFitWidth(60.0);
+			alert.setResizable(true);
 			alert.showAndWait();
 		} else if (text.equals("Tutorial")) {
 			Alert alert = new Alert(AlertType.INFORMATION, "Each player is assign to a disk's color.\nThe first player is the yellow one.\n\nEach player takes turns dropping one colored disc\nfrom the top into a seven-column,\nsix-row vertically suspended grid.\n\nThe pieces fall straight down, occupying\nthe next available space within the column.\n\nThe objective of the game is to be\nthe first to form a horizontal,\nvertical, or diagonal line of\nfour of one's own discs.");
