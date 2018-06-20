878104652cd3c465dfb2722a721c42820400a839
Edwin CHARLOTTE <zed45000@gmail.com>
2018-06-20 11:22:14 +0200

    ajout des fichiers debut pour fiche jeu

--- code/jeu/src/application/BorderFicheJeu.java
+++ code/jeu/src/application/BorderFicheJeu.java
@@ -8,7 +8,7 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
-public class BorderFicheJeu extends PageAdmin {
+public class BorderFicheJeu extends PageJoueur {
 
     AppliJDBC app;
 
@@ -16,30 +16,25 @@ public class BorderFicheJeu extends PageAdmin {
         super();
         this.app = app;
 
-        Button bMenu = this.buttonTypePageAdmin("Accueil");
-        bMenu.setOnAction(event -> this.app.passerEnModeAccueil());
-
-        Button bEnregistrer = this.buttonTypePageAdmin("Enregistrer");
-        bEnregistrer.setOnAction(event -> this.app.passerEnModeGererJeux());
 
         GridPane cotegauche = new GridPane();
 
-        cotegauche.add(this.labelTypePageAdmin("Titre :"),0,0);
-        cotegauche.add(this.labelTypePageAdmin("<Titre>"),1,0);
-        cotegauche.add(this.labelTypePageAdmin("Type de jeu : "),0,4);
-        cotegauche.add(this.comboBoxAdmin("TpT","Simultané"),1,4);
+        cotegauche.add(this.labelTypePageJoueur("Titre :"),0,0);
+        cotegauche.add(this.labelTypePageJoueur("<Titre>"),1,0);
+        cotegauche.add(this.labelTypePageJoueur("Type de jeu : "),0,4);
+        cotegauche.add(this.labelTypePageJoueur(""),1,4);
         cotegauche.setPadding(new Insets(30, 30, 10, 10));
 
         GridPane cotehaut= new GridPane();
 
-        cotehaut.add(this.imageTypePageAdmin(),0,0);
+        // cotehaut.add(this.imageTypePageAdmin(),0,0);
         cotehaut.add(cotegauche,1,0);
         cotehaut.setPadding(new Insets(30, 30, 10, 10));
 
         GridPane cotebas = new GridPane();
 
 
-        cotebas.add(this.labelTypePageAdmin("Description : "),0,1);
+        cotebas.add(this.labelTypePageJoueur("Description : "),0,1);
         cotebas.add(this.textAeraHorizontalNonEditable(),1,1);
         cotebas.setPadding(new Insets(30, 30, 10, 10));
 
@@ -47,7 +42,6 @@ public class BorderFicheJeu extends PageAdmin {
 
         this.setTop(cotehaut);
         this.setCenter(cotebas);
-        this.setBottom(this.buttonBarTypePageAdmin(bMenu, bEnregistrer));
 
 
         this.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
