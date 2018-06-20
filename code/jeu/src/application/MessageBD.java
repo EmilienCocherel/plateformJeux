e7bf59c6e76e5d24d4dcc7560bb9603acf58cebe
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-20 14:53:29 +0200

    APPLICATION: MessageBD ajouter listeDesMessagesRecusParJoueur() et listeDesMessagesEnvoyesParJour()

--- code/jeu/src/application/MessageBD.java
+++ code/jeu/src/application/MessageBD.java
@@ -10,6 +10,7 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MessageBD {
 	private ConnexionMySQL laConnexion;
@@ -45,7 +46,7 @@ public class MessageBD {
 		PreparedStatement ps = laConnexion.prepareStatement("insert into MESSAGE values (?,?,?,?,?,?)");
 		int id = this.maxId() + 1;
 		String contenu = m.getObjet()+"\n"+m.getMessage(),
-			   lu = m.isLu() ? "O" : "N";
+			   lu = m.getLu() ? "O" : "N";
 		ps.setInt(1, id);
 		ps.setTimestamp(2, new Timestamp(m.getDate().getTime()));
 		ps.setString(3, contenu);
@@ -55,5 +56,55 @@ public class MessageBD {
 		ps.executeUpdate();
 		m.setId(id);
 		return id;
+	}
+
+	/**
+	 * @param joueur Le joueur
+	 * @return les messages reçus par le joueur
+	 */
+	public List<Message> listeDesMessagesRecusParJoueur(Joueur joueur) throws SQLException {
+		List<Message> liste = new ArrayList<>();
+		PreparedStatement ps = laConnexion.prepareStatement("Select * from MESSAGE where idUt2 = ?");
+		ps.setInt(1, joueur.getIdentifiant());
+		ResultSet res = ps.executeQuery();
+		while (res.next()) {
+			int idMsg = res.getInt("idMsg");
+			Joueur joueur1 = this.joueurBD.rechercherJoueurParNum(res.getInt("idUt1"));
+			Date dateMsg = res.getTimestamp("dateMsg");
+			List<String> contenu = Arrays.asList(res.getString("contenuMsg").split("\n"));
+			String objet = contenu.get(0),
+				   message = String.join("\n", contenu.subList(1, contenu.size()-1));
+			boolean luMsg = res.getString("luMsg").equals("O") ? true : false;
+
+			liste.add(new Message(idMsg, dateMsg, objet, message, luMsg, joueur, joueur1));
+		}
+		res.close();
+		System.out.println(liste);
+		return liste;
+	}
+
+	/**
+	 * @param joueur Le joueur
+	 * @return les messages envoyés par le joueur
+	 */
+	public List<Message> listeDesMessagesEnvoyesParJoueur(Joueur joueur) throws SQLException {
+		List<Message> liste = new ArrayList<>();
+		PreparedStatement ps = laConnexion.prepareStatement("Select * from MESSAGE where idUt1 = ?");
+		ps.setInt(1, joueur.getIdentifiant());
+		ResultSet res = ps.executeQuery();
+		while (res.next()) {
+			int idMsg = res.getInt("idMsg");
+			Joueur joueur2 = this.joueurBD.rechercherJoueurParNum(res.getInt("idUt2"));
+			Date dateMsg = res.getTimestamp("dateMsg");
+			List<String> contenu = Arrays.asList(res.getString("contenuMsg").split("\n"));
+			String objet = contenu.get(0),
+				   message = String.join("\n", contenu.subList(1, contenu.size()-1));
+			boolean luMsg = res.getString("luMsg").equals("O") ? true : false;
+
+			liste.add(new Message(idMsg, dateMsg, objet, message, luMsg, joueur2, joueur));
+		}
+		res.close();
+		System.out.println(liste);
+		return liste;
 	}
 }
