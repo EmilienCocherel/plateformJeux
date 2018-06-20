c6aa8c35e955e75b187565a45eb956f227a64f14
Cocherel <emilien.cocherel@gmail.com>
2018-06-20 08:45:10 +0200

    fix

--- code/jeu/src/exeAppMastermind.sh
+++ code/jeu/src/exeAppMastermind.sh
@@ -1,6 +1,9 @@
 javac -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application/*.java
 javac -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar mastermind/*.java
+javac -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar connect4/*.java
+
 jar -cvf morpion.jar mastermind/*.class
-java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.MettreJeuDansBD servinfo-db "$1" "$2" "$3" mastermind mastermind.jar
+jar -cvf morpion.jar connect4/*.class
 java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.TelechargerJeu servinfo-db "$1" "$2" "$3" mastermind jar/mastermind.jar
+java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.TelechargerJeu servinfo-db "$1" "$2" "$3" connect4 jar/connect4.jar
 java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.AppliJDBC
