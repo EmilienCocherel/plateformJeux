7f6f28bea1e36389bf9d5bc059c06695b4cd12b8
Edwin CHARLOTTE <zed45000@gmail.com>
2018-06-20 11:10:05 +0200

    remise a niveau

--- code/jeu/src/exeApp.sh
+++ code/jeu/src/exeApp.sh
@@ -3,6 +3,8 @@ javac -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar mastermind/*.java
 javac -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar connect4/*.java
 jar -cvf connect4.jar connect4/*.class connect4/style/style.css
 jar -cvf morpion.jar mastermind/*.class
+mysql -h servinfo-db -u charlotte -D dbcharlotte -p -e "delete from PARTIE"
+mysql -h servinfo-db -u charlotte -D dbcharlotte -p -e "delete from JEU"
 java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.MettreJeuDansBD servinfo-db "$1" "$2" "$3" mastermind mastermind.jar
 java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.TelechargerJeu servinfo-db "$1" "$2" "$3" mastermind jar/mastermind.jar
 java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.MettreJeuDansBD servinfo-db "$1" "$2" "$3" connect4 connect4.jar
