700dad9e5466438aaa688a8d1d6c23511b586641
Amélie Dauvois <amelie.dauvois@etu.univ-orleans.fr>
2018-06-21 16:01:45 +0200

    APPLICATION: affichage des stats (en théorie)

--- code/jeu/src/application/BorderConsutlerStatsJeux.java
+++ code/jeu/src/application/BorderConsutlerStatsJeux.java
@@ -1,14 +1,19 @@
 package application;
 
-
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.sql.SQLException;
 
 public class BorderConsutlerStatsJeux extends PageJoueur {
-
-    AppliJDBC app;
+    private AppliJDBC app;
+	private TableView<Statistique> tableau;
 
     public BorderConsutlerStatsJeux(AppliJDBC app){
         super();
@@ -17,7 +22,7 @@ public class BorderConsutlerStatsJeux extends PageJoueur {
         Button brevenir = this.buttonTypePageJoueur("Revenir au profil");
         brevenir.setOnAction(event -> this.app.passerEnModeProfilUtilisateur());
 
-        HBox haut = this.hboxTypePageJoueur("Statistiques de <Nom joueur>     ", brevenir);
+        HBox haut = this.hboxTypePageJoueur("Statistiques de "+this.app.getClient().getPseudo(), brevenir);
 
         Button jeux = this.buttonTypePageJoueur("Jeux");
         jeux.setOnAction(event -> this.app.passerEnModeConsulterStatsJeux());
@@ -27,7 +32,28 @@ public class BorderConsutlerStatsJeux extends PageJoueur {
 
         BorderPane centre = new BorderPane();
         centre.setTop(this.buttonBarTypePageJoueur(jeux,adversaire));
-        centre.setCenter(this.tableauTypePageJouer("Nom jeu","% de victoires","Score Moyen"));
+
+
+        this.tableau = new TableView<>();
+		ObservableList<Statistique> liste;
+		try {
+			liste = FXCollections.observableList(
+					this.app.getPartieBD().listeStatsParJeu(
+						this.app.getClient()
+						));
+		} catch (SQLException ex) {
+			liste = FXCollections.emptyObservableList();
+		}
+		tableau.setItems(liste);
+
+		TableColumn<Statistique,String> date = new TableColumn<>("Nom du jeu");
+		date.setCellValueFactory(new PropertyValueFactory("nomJeu"));
+		TableColumn<Statistique,String> objet = new TableColumn<>("% de victoires");
+		objet.setCellValueFactory(new PropertyValueFactory("pourcentageVictoire"));
+
+		tableau.getColumns().setAll(date, objet);
+        tableau.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        centre.setCenter(this.tableau);
 
         this.setMaxSize(800, 700);
         this.setBackground(new Background(new BackgroundFill(Color.rgb(53, 56, 61), new CornerRadii(5, false), Insets.EMPTY)));
@@ -35,7 +61,5 @@ public class BorderConsutlerStatsJeux extends PageJoueur {
 
         this.setTop(haut);
         this.setCenter(centre);
-
-
     }
 }
