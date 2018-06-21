4b155e42ee5d5cf564439677c618d7b8e3799e8c
Etienne <cp-eti2@hotmail.f>
2018-06-21 16:26:37 +0200

    correction launch.sh

--- code/jeu/src/launch.sh
+++ code/jeu/src/launch.sh
@@ -1,4 +1,4 @@
 javac -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application/*.java
 java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.TelechargerJeu servinfo-db dbcharlotte charlotte charlotte mastermind jar/mastermind.jar
-java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.TelechargerJeu servinfo-db charlotte charlotte charlotte connect4 jar/connect4.jar
+java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.TelechargerJeu servinfo-db dbcharlotte charlotte charlotte connect4 jar/connect4.jar
 java -cp .:/pub/1A/json-simple.jar:/usr/share/java/mysql.jar application.AppliJDBC
